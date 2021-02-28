a2db45b7636c84b8a8ce35fa82f762e00c5fac10
migue0418 <55736829+migue0418@users.noreply.github.com>
2021-02-28 11:30:01 +0100

    Home Section Completed

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Portfolio Web
  This is my personal webpage with my curriculum.
+ I made all from scratch using HTML, CSS and JavaScript.
--- functions.js
+++ functions.js
@@ -1,3 +1,45 @@
+// Home Slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function moveSlides(n) {
+    slideIndex += n;
+    var slides = document.getElementsByClassName("slides");
+    
+    if(slideIndex > slides.length){
+        slideIndex = 1;
+    }
+
+    if(slideIndex < 1){
+        slideIndex = slides.length;
+    }
+
+    showSlides();
+}
+
+function gotoSlide(n) {
+    slideIndex = n;
+    showSlides();
+}
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("dot");
+
+    for (i=0; i<slides.length; i++){
+        slides[i].classList.add('hidden');
+    }
+
+    for (i=0; i<dots.length; i++){
+        dots[i].classList.remove('active');
+    }
+
+    slides[slideIndex-1].classList.remove('hidden');
+    dots[slideIndex-1].classList.add('active');
+}
+
+// Resume tabs
 function tabShowHide(idContent, idHeader){
     var tabs_header =["tabEducation", "tabCoding", "tabSoftware"];
     var tabs_content =["education", "coding", "software"];
